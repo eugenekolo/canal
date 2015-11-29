a1b79575856808371ef3eea1a5d37c69723e86a9
sahinonur <sahin@bu.edu>
2015-11-29 18:32:46 -0500

    Added sample cflow files

--- src/cflow2png.sh
+++ src/cflow2png.sh
@@ -3,22 +3,42 @@
 # A small script to convert cflow
 # output into png images
 
-# @input-1: source .cflow file
-# @output: png image
+# @Input	: a path to source .cflow file
+# @Output	: a flow diagram as a png image
 
 # Dependencies:
 #	dot
 #	cflow2dot
 
 
+####################
+# Helper Functions #
+####################
+# Input and Environment Check/Validation
+error_checklist(){
+
+	if [ ! -f "$SOURCE_FILE_PATH" ]; then
+		echo "[cflow2png]: Invalid path to source cflow file."
+		echo "[cflow2png]: Usage: ./cflow2png [PATH TO '.cflow; FILE]"
+		exit
+	fi
+}
+
+####################
+# MAIN STARTS HERE #
+####################
+
 # Set target path for storing the PNG images
 DEFAULT_PATH="./"
 : ${OUTPUT_PNG_PATH="$DEFAULT_PATH"}
 
-# Convert to PNG
 SOURCE_FILE_PATH="$1"
-OUTPUT_IMAGE_NAME=${SOURCE_FILE_PATH##*/} # last field in the path
 
+# Error checks
+error_checklist
+
+# Plot flow diagram and save as a PNG image
+OUTPUT_IMAGE_NAME=${SOURCE_FILE_PATH##*/} # last field in the path
 cat $SOURCE_FILE_PATH | cflow2dot | dot > $SOURCE_FILE_PATH".dot"
 
 dot -Tpng $SOURCE_FILE_PATH".dot" > "$OUTPUT_PNG_PATH/""$OUTPUT_IMAGE_NAME"".png"
