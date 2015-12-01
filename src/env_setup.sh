036bbb1e0a02bb11fcec66f539d73ec3f36c7665
sahinonur <sahin@bu.edu>
2015-12-01 12:31:17 -0500

    Return checker submodule wrapper added.

--- src/env_setup.sh
+++ src/env_setup.sh
@@ -18,7 +18,8 @@ fi
 if [ $1 == "0" ]; then
         echo "[EnvSetup]: Clearing environment."
         unset CFLOW_OUTPUT_DIRECTORY CFLOW_INPUT_PROJECT_DIRECTORY CFLOW_GENERATION_ENABLED \
-                VISUALIZATION_ENABLED VULNERABILITY_ANALYSIS_ENABLED
+		VISUALIZATION_ENABLED OUTPUT_PNG_PATH KEEP_DOT_FILES \
+      		VULNERABILITY_ANALYSIS_ENABLED VULNERABILITY_ANALYSIS_TYPE1_ENABLED VULNERABILITY_ANALYSIS_TYPE2_ENABLED VULNERABILITY_ANALYSIS_TYPE3_ENABLED
 	return
 fi
 
@@ -35,16 +36,12 @@ export CFLOW_GENERATION_ENABLED=1
 # Vulnerability Analysis Options #
 ##################################
 
-#export VULNERABILITY_ANALYSIS_ENABLED=1
+export VULNERABILITY_ANALYSIS_ENABLED=1
 #export VULNERABILITY_ANALYSIS_TYPE1_ENABLED=1 	# Checking occurance of A before B
-#export VULNERABILITY_ANALYSIS_TYPE2_ENABLED=1	# Checking of function call returns
+export VULNERABILITY_ANALYSIS_TYPE2_ENABLED=1	# Checking of function call returns
 #export VULNERABILITY_ANALYSIS_TYPE3_ENABLED=1 	# Checking of exec* or system calls with known vulnerable binaries
 
-###################
-# Logging Options #
-###################
-
-
+export VULNERABILITY_LOGS_DIRECTORY="$HOME/GoogleDrive/Courses/EC521/Project/logs/"
 
 ###############################
 # Graph Visualization Options #
@@ -52,9 +49,7 @@ export CFLOW_GENERATION_ENABLED=1
 
 # Enable flow visualization?
 export VISUALIZATION_ENABLED=1
-
 # Where to store .png images for call graphs
 export OUTPUT_PNG_PATH="$HOME/GoogleDrive/Courses/EC521/Project/Cflow_Images"
-
 # Keep (or remove) .dot (graph) files?
 export KEEP_DOT_FILES=0
--- src/runSubmodules.sh
+++ src/runSubmodules.sh
@@ -1,10 +1,71 @@
 #!/bin/bash
 
-# TODO-1: Call "A before B" submodule with every .cflow file
-# Discuss with Eugene about how to handle/log output
+#################### INFO #########################
+# This is a wrapper script to run each
+# vulnerability check submodule against
+# target set of programs.
 
-# TODO-2: Call "return check" submodule with every c file
-# Discuss with Josh about how to hangle/log output
+# HOW TO RUN:
+# This script is called by the main vulnAnalysis.sh:
+#	source env_setup.sh 1
+#	chmod 744 runSubmodules.sh; ./runSubmodules
+####################################################
 
-# TODO-3: Call "execve checker" submodule with every c file
-# Discuss with Max/Dreq about how to handle/log output 
+# We do not run any submodules by default
+: ${VULNERABILITY_ANALYSIS_TYPE1_ENABLED=0} # A before B
+: ${VULNERABILITY_ANALYSIS_TYPE2_ENABLED=0} # return check
+: ${VULNERABILITY_ANALYSIS_TYPE3_ENABLED=0} # execve checker
+
+# Input and Environment Check/Validation
+error_checklist(){
+
+        if [ ! -d "$CFLOW_INPUT_PROJECT_DIRECTORY" ]; then
+                echo "[Run Submodules]: Invalid Project Source Directory."
+                exit
+        fi
+
+        if [ ! -d "$VULNERABILITY_LOGS_DIRECTORY" ]; then
+                echo "[Run Submodules]: Invalid directory to store vulnerability logs."
+                exit
+        fi
+}
+
+return_check_wrapper(){
+	scripts_dir=`pwd`
+	cd $CFLOW_INPUT_PROJECT_DIRECTORY
+
+	# Get a copy of the submodule code
+	cp $scripts_dir/returnCheckerSubmodule.py .
+
+	#  Get a list of all the c programs
+	LIST_OF_C_PROGRAMS=`find ./ -iname "*.c"`
+	#echo $LIST_OF_C_PROGRAMS
+
+	# Remove any previously existing log file
+	rm $VULNERABILITY_LOGS_DIRECTORY/VULNERABILITIES_RETURN_CHECKS.log 2>/dev/null
+
+	# Feed every c program to vulnerability checker submodule
+	for program in $LIST_OF_C_PROGRAMS; do
+		python returnCheckerSubmodule.py $program 1>>$VULNERABILITY_LOGS_DIRECTORY/VULNERABILITIES_RETURN_CHECKS.log 2>>$VULNERABILITY_LOGS_DIRECTORY/ERRORS.log
+	done
+
+	rm returnCheckerSubmodule.py
+	cd $scripts_dir
+}
+
+a_before_b_wrapper(){
+	echo "TODO-1: Call A before B submodule with every .cflow file"
+	echo "Discuss with Eugene about how to handle/log output"
+}
+
+execve_wrapper(){
+
+	echo "TODO-3: Call execve checker submodule with every c file"
+	echo "Discuss with Josh about how to handle/log output"
+}
+
+# TODO: Call each wrapper depending on the options
+
+if [ $VULNERABILITY_ANALYSIS_TYPE2_ENABLED == 1 ]; then
+	return_check_wrapper
+fi
